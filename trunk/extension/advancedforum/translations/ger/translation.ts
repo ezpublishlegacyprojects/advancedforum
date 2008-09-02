3aa2646cba77cc8192af06133b781eba6c14b1ca
Björn Dieding <bjoern@xrow.de>
2008-09-02 14:48:49 +0000

    fixing translations
    git-svn-id: http://svn.projects.ez.no/advancedforum@6 8ec4f5af-ffc5-4ad0-b059-f49c51e908ed

--- trunk/extension/advancedforum/translations/ger/translation.ts
+++ trunk/extension/advancedforum/translations/ger/translation.ts
@@ -11,58 +11,47 @@ Knotensignalisierung</translated>
     <message>
       <source>Name</source>
       <translation>Name</translation>
-      <translated>Name</translated>
     </message>
     <message>
       <source>Class</source>
       <translation>Klasse</translation>
-      <translated>Klasse</translated>
     </message>
     <message>
       <source>Select</source>
       <translation>Auswahl</translation>
-      <translated>Auswahl</translated>
     </message>
     <message>
       <source>Remove</source>
       <translation>Entfernen</translation>
-      <translated>Entfernen</translated>
     </message>
     <message>
       <source>Re</source>
       <translation>Betreff</translation>
-      <translated>Betreff</translated>
     </message>
     <message>
       <source>The full forum thread can be read at</source>
       <translation>Vollständiges Thema:</translation>
-      <translated>Vollständiges Thema:</translated>
     </message>
     <message>
       <source>If you do not wish to continue receiving these notifications,
 change your settings at:</source>
       <translation>Wenn Sie diese Benachrichtigungen nicht mehr erhalten möchten, ändern Sie Ihre Einstellungen:</translation>
-      <translated>Wenn Sie diese Benachrichtigungen nicht mehr erhalten möchten, ändern Sie Ihre Einstellungen:</translated>
     </message>
     <message>
       <source>%sitename notification system</source>
       <translation>%sitename-Benachrichtigungssystem</translation>
-      <translated>%sitename-Benachrichtigungssystem</translated>
     </message>
     <message>
       <source>Notification settings</source>
       <translation>Benachrichtigungen</translation>
-      <translated>Benachrichtigungen</translated>
     </message>
     <message>
       <source>Store</source>
       <translation>Speichern</translation>
-      <translated>Speichern</translated>
     </message>
     <message>
       <source>Cancel</source>
       <translation>Abbrechen</translation>
-      <translated>Abbrechen</translated>
     </message>
   </context>
   <context>
@@ -70,182 +59,146 @@ change your settings at:</source>
     <message>
       <source>Edit %1 - %2</source>
       <translation>%1 - %2 bearbeiten</translation>
-      <translated>%1 - %2 bearbeiten</translated>
     </message>
     <message>
       <source>Send for publishing</source>
       <translation>Veröffentlichen</translation>
-      <translated>Veröffentlichen</translated>
     </message>
     <message>
       <source>Discard</source>
       <translation>Verwerfen</translation>
-      <translated>Verwerfen</translated>
     </message>
     <message>
       <source>Subject</source>
       <translation>Thema</translation>
-      <translated>Thema</translated>
     </message>
     <message>
       <source>Message</source>
       <translation>Mitteilung</translation>
-      <translated>Mitteilung</translated>
     </message>
     <message>
       <source>Sticky</source>
       <translation>Fixiert</translation>
-      <translated>Fixiert</translated>
     </message>
     <message>
       <source>You need to be logged in to get access to the forums. You can do so %login_link_start%here%login_link_end%.</source>
       <translation>Sie müssen angemeldet sein, um Beiträge zu verfassen. Melden Sie sich %login_link_start%hier%login_link_end% an.</translation>
-      <translated>Sie müssen angemeldet sein, um Beiträge zu verfassen. Melden Sie sich %login_link_start%hier%login_link_end% an.</translated>
     </message>
     <message>
       <source>New topic</source>
       <translation>Neues Thema</translation>
-      <translated>Neues Thema</translated>
     </message>
     <message>
       <source>Search</source>
       <translation>Suche</translation>
-      <translated>Suche</translated>
     </message>
     <message>
       <source>Keep me updated</source>
       <translation>Über Aktualisierungen benachrichtigen</translation>
-      <translated>Über Aktualisierungen benachrichtigen</translated>
     </message>
     <message>
       <source>Neues</source>
       <translation>Neues</translation>
-      <translated>Neues</translated>
     </message>
     <message>
       <source>Topic</source>
       <translation>Thema</translation>
-      <translated>Thema</translated>
     </message>
     <message>
       <source>Replies</source>
       <translation>Antworten</translation>
-      <translated>Antworten</translated>
     </message>
     <message>
       <source>Views</source>
       <translation>Abrufe</translation>
-      <translated>Abrufe</translated>
     </message>
     <message>
       <source>Author</source>
       <translation>von</translation>
-      <translated>von</translated>
     </message>
     <message>
       <source>Last reply</source>
       <translation>Letzte Antwort</translation>
-      <translated>Letzte Antwort</translated>
     </message>
     <message>
       <source>Pages</source>
       <translation>Seiten</translation>
-      <translated>Seiten</translated>
     </message>
     <message>
       <source>by</source>
       <translation>von</translation>
-      <translated>von</translated>
     </message>
     <message>
       <source>Message preview</source>
       <translation>Vorschau der Nachricht</translation>
-      <translated>Vorschau der Nachricht</translated>
     </message>
     <message>
       <source>Location</source>
       <translation>Ort</translation>
-      <translated>Ort</translated>
     </message>
     <message>
       <source>Moderated by</source>
       <translation>Moderator</translation>
-      <translated>Moderator</translated>
     </message>
     <message>
       <source>Edit</source>
       <translation>Bearbeiten</translation>
-      <translated>Bearbeiten</translated>
     </message>
     <message>
       <source>Previous topic</source>
       <translation>Vorheriges Thema</translation>
-      <translated>Vorheriges Thema</translated>
     </message>
     <message>
       <source>Next topic</source>
       <translation>Nächstes Thema</translation>
-      <translated>Nächstes Thema</translated>
     </message>
     <message>
       <source>New reply</source>
       <translation>Neue Antwort</translation>
-      <translated>Neue Antwort</translated>
     </message>
     <message>
       <source>Posts</source>
       <translation>Beiträge</translation>
-      <translated>Beiträge</translated>
     </message>
     <message>
       <source>Edit forum message</source>
       <translation>Forum-Nachricht bearbeiten</translation>
-      <translated>Forum-Nachricht bearbeiten</translated>
     </message>
     <message>
       <source>Profile</source>
       <translation>Nutzerprofil</translation>
-      <translated>Nutzerprofil</translated>
     </message>
     <message>
       <source>View profile</source>
       <translation>Nutzerprofil ansehen</translation>
-      <translated>Nutzerprofil ansehen</translated>
     </message>
     <message>
       <source>Forum</source>
       <translation>Forum</translation>
-      <translated>Forum</translated>
     </message>
     <message>
       <source>Last post</source>
       <translation>Letzte Veröffentlichung</translation>
-      <translated>Letzte Veröffentlichung</translated>
     </message>
     <message>
       <source>Posted on %date by %user</source>
       <translation>Veröffentlicht am %date von %user</translation>
-      <translated>Veröffentlicht am %date von %user</translated>
     </message>
     <message>
       <source>You have to login to use this feature.</source>
       <translation>Sie müssen angemeldet sein, um diese Funktion zu nutzen.</translation>
-      <translated>Sie müssen angemeldet sein, um diese Funktion zu nutzen.</translated>
     </message>
     <message>
       <source>New content since last visit</source>
       <translation>Neue Beiträge seit dem letzten Besuch</translation>
-      <translated>Neue Beiträge seit dem letzten Besuch</translated>
     </message>
     <message>
       <source>Your last visit to this site was</source>
       <translation>Ihr letzter Besuch war am</translation>
-      <translated>Ihr letzter Besuch war am</translated>
     </message>
     <message>
       <source>There is no new content since your last visit.</source>
       <translation>Keine neuen Beiträge seit Ihrem letzten Besuch.</translation>
-      <translated>Keine neuen Beiträge seit Ihrem letzten Besuch.</translated>
     </message>
   </context>
 </TS>
